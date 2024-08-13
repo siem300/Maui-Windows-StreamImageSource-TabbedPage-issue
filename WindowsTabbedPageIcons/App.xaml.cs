828c8c1233ef46e9308e2aaefa9e6aaebb6fdc72
siem300 <svdkluft@gmail.com>
2024-08-13 15:36:02 +0200

    Add files via upload

---
+++ WindowsTabbedPageIcons/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WindowsTabbedPageIcons
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ WindowsTabbedPageIcons/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace WindowsTabbedPageIcons
+{
+    public partial class MainPage : TabbedPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            var assembly = Assembly.GetExecutingAssembly();
+            Stream? stream = assembly.GetManifestResourceStream($"WindowsTabbedPageIcons.Resources.Images.dotnet_bot_embedded.png");
+            ImageSource = ImageSource.FromStream(() => stream);
+        }
+
+        private ImageSource _imageSource;
+        public ImageSource ImageSource 
+        {
+            get => _imageSource;
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+    }
+
+}
